c7f0a5237e1112963e6d380d2c5f63f1083b16ab
kayzou <elghazzalyzakaria@gmail.com>
2024-07-15 20:31:16 +0100

    setup prima & user post api

--- tqda-backend/src/users/users.service.ts
+++ tqda-backend/src/users/users.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.req';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequest) {
-    console.log(data);
+  constructor(private readonly prismaService: PrismaService) {}
+  async createUser(data: CreateUserRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: { ...data, password: await bcrypt.hash(data.password, 10) },
+        select: { email: true, id: true },
+      });
+    } catch (error) {
+      console.log(error);
+      if (error.code === 'P2002')
+        throw new UnprocessableEntityException('Email already exists!');
+      throw error;
+    }
   }
 }
