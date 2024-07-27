b60310fd206870167cb0226312fbf70e42c4adeb
kayzou <elghazzalyzakaria@gmail.com>
2024-07-27 18:24:16 +0100

    setup stripe checkout session api

--- tqda-backend/src/products/products.module.ts
+++ tqda-backend/src/products/products.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
